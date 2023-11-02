752e4043a8b2e438f548fa18622a8b7c9c0bf373
Shakir Abdo <shicolare1@gmail.com>
2023-11-02 09:43:45 +0300

    add 0-add.py

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """addition function
+
+    args:
+        a: the first integer
+        b: the second integer
+
+    returns:
+        return value. a + b
+    """
+    return (a + b)
