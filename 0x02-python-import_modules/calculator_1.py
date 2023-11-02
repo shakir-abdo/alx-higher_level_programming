7d5680fd2e941b0a3cc1ff457eb5f096b64783cb
Shakir Abdo <shicolare1@gmail.com>
2023-11-02 09:46:25 +0300

    add 1-calculation.py

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """addition function
+
+    args:
+        a: the first integer
+        b: the second integer
+
+    returns:
+        return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """subtraction function"""
+    return (a - b)
+
+
+def mul(a, b):
+    """multiplication function"""
+    return (a * b)
+
+
+def div(a, b):
+    """division function"""
+    return int(a / b)
