16a119651cafa556de0b1d77aba2e6fe0a028a4d
Shakir Abdo <shicolare1@gmail.com>
2023-11-22 02:30:09 +0300

    fix 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Class Square definition"""
+
+
+class Square:
+    """represents a square"""
+
+    def __init__(self, size):
+        """Create new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
