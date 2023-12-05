1b90f4e7570c48a768a29bc90b837471514e9210
Shakir Abdo <shicolare1@gmail.com>
2023-12-05 03:38:21 +0300

    fix 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,4 +1,4 @@
-"""a Rectangle subclass Square."""
+""" a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
@@ -6,8 +6,7 @@ class Square(Rectangle):
     """Represent a square."""
 
     def __init__(self, size):
-        """
-        Initialize a new square.
+        """Initialize a new square.
 
         Args:
             size (int): The size of the new square.
