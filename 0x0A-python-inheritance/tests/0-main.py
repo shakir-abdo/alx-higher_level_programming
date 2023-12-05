6d38473070b80af766d3f7c873ce3e7d0ea67982
Shakir Abdo <shicolare1@gmail.com>
2023-12-05 03:13:07 +0300

    add 1-my_list.py, tests/1-my_list.txt

--- 0x0A-python-inheritance/tests/0-main.py
+++ 0x0A-python-inheritance/tests/0-main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 lookup = __import__('0-lookup').lookup
 
-class FirstClass(object):
+class MyClass1(object):
     pass
 
-class SecondClass(object):
+class MyClass2(object):
     my_attr1 = 3
     def my_meth(self):
         pass
 
-print(lookup(FirstClass))
-print(lookup(SecondClass))
+print(lookup(MyClass1))
+print(lookup(MyClass2))
 print(lookup(int))
