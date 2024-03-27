5b519ea5cc1f50cfdb7fa31311ee7d5bcfe70180
Shakir Abdo <shicolare1@gmail.com>
2024-03-27 20:26:15 +0300

    fix task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
     if (error) {
-        console.log(error)
+        console.error(error)
         return
     }
   console.log(content)
