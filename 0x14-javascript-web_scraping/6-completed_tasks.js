e09ac0d97e29bce80532f9338dd5fc4e27442253
Shakir Abdo <shicolare1@gmail.com>
2024-03-27 20:37:03 +0300

    add task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else if (res.statusCode === 200) {
+    const tasks = JSON.parse(body);
+    const completed = {};
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + res.statusCode);
+  }
+});
